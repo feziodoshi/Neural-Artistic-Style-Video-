d627634e6834e680fc220bd12e877b5e576fea66
Raghav Gupta <raghav0296@gmail.com>
2017-02-26 23:49:22 +0530

    add comments for documentation

--- videoCapture.py
+++ videoCapture.py
@@ -1,5 +1,6 @@
 import cv2
 
+# the no. of seconds the video has to be and the frames-per-sec of the video
 num_secs = 5
 fps = 20.0
 
@@ -14,12 +15,13 @@ print (webcamReso)
 # Above line for opencv version 3...below line for opencv version 2.x
 # fourcc = cv2.cv.CV_FOURCC(*"DIVX") #or XVID
 
+# VideoWriter for writing the frames into a video
+out = cv2.VideoWriter('VidPorn.avi',-1, fps, webcamReso)
 # out = cv2.VideoWriter('VidPron.avi',fourcc, 20.0, (webcam.get(cv2.CV_CAP_PROP_FRAME_WIDTH),webcam.get(cv2.CV_CAP_PROP_FRAME_WIDTH)))
 # Above line for opencv version 3...below line for opencv version 2.x
 
-out = cv2.VideoWriter('VidPorn.avi',-1, fps, webcamReso)
-
 no_frames = num_secs*fps
+# ctr for counting the no. of frames inside while loop
 ctr = 0
 
 while(webcam.isOpened()):
@@ -28,7 +30,7 @@ while(webcam.isOpened()):
     ctr += 1
     frame = cv2.flip(frame,1)
     out.write(frame)
-    cv2.imshow('VidPorn',frame)
+    cv2.imshow('neural_style_1',frame)
 
     if cv2.waitKey(1) == 27 or (ctr>no_frames):   #ESC
         break
